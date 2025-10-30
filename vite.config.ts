966856bf8794c8e62b2938cddc7b1f5ac73bd6b2
pedrabrancagames <pedrabrancagames@gmail.com>
2025-10-30 17:52:55 -0300

    fix: Remove a definição da GEMINI_API_KEY do vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,10 +18,7 @@ export default defineConfig(({ mode }) => {
       css: {
         postcss: './postcss.config.js',
       },
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
