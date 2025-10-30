19b167b0ece0312e2c200eff901ecc8ec04fe699
pedrabrancagames <pedrabrancagames@gmail.com>
2025-10-30 18:00:27 -0300

    fix: Simplifica a configuração de conteúdo do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./App.tsx",
-    "./*.{js,ts,jsx,tsx}"
+    "./**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
